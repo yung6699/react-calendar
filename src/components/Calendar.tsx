ec960c8e8daa1eeff2419d5cbd5c1bea3ea1acf0
Yoon <yty3592@gmail.com>
2020-12-29 01:15:07 +0900

    react calendar project init setting
    - eslint 설정
    - prettierrc 설정
    - 프로젝트 구조 잡기

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <div>
+      <h1>달력입니다.</h1>
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ src/container/CalendarContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "@/components/Calendar";
+
+const CalendarContainer = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default CalendarContainer;
