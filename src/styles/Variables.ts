adfc5a55e2e53d277ac0e2371fdcbef454a278f4
Yoon <yty3592@gmail.com>
2020-12-30 01:29:00 +0900

    스타일관련 클래스명 Variables.ts에 추가

--- src/styles/Variables.ts
+++ src/styles/Variables.ts
@@ -1,2 +1,5 @@
 export const CALENDAR_ITEM_WIDTH = "14.285714286%";
 export const CALENDAR_ITEM_HEIGHT = "50px";
+export const SELECTED_CLASS_NAME = "selected";
+export const SUNDAY_CLASS_NAME = "sun";
+export const SATURDAY_CLASS_NAME = "sat";
