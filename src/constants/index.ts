3cebd6a1912271a5436af5378a2f47e29b3f6eac
Yoon <yty3592@gmail.com>
2020-12-29 20:53:39 +0900

    constants 정리

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,20 @@
-export const DAY_OF_THE_WEEK: string[] = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+export const DAY_OF_THE_WEEK: string[] = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+export const SUNDAY = 0;
+export const SATURDAY = 6;
+export const MONTHS: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export const END_DAYS: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 export const FIRST_MONTH = 1;
 export const LAST_MONTH = 12;
