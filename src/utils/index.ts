a88294c57ff26dba709cf2232262926b15032f0e
Yoon <yty3592@gmail.com>
2020-12-30 01:29:19 +0900

    날짜관련 Utils 함수 정리

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
-import { END_DAYS, SUNDAY, SATURDAY } from "../constants";
+import { END_DAYS, SUNDAY, SATURDAY, DAY_BOX_COUNT, LINE_COUNT, MONTH_LIST } from "../constants";
+import { SUNDAY_CLASS_NAME, SATURDAY_CLASS_NAME } from "../styles/Variables";
 
 export const isLeapYear = (year: number) => {
   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
@@ -21,8 +22,33 @@ export const getFirstDayIndexOfWeek = (year: number, month: number) => {
   return days % 7;
 };
 
+export const getMonthName = (month: number) => {
+  return MONTH_LIST[month].substring(0, 3).toUpperCase();
+};
+
+export const getDayRows = (year: number, month: number) => {
+  const endDays = getEndDays(year);
+  const lastDay = endDays[month - 1];
+  const firstDayIndexOfWeek = getFirstDayIndexOfWeek(year, month); // 선택된 달의 1일의 요일을 찾는다.
+  const days = new Array(DAY_BOX_COUNT).fill(null);
+
+  for (let i = firstDayIndexOfWeek; i < lastDay + firstDayIndexOfWeek; i++) {
+    days[i] = i - firstDayIndexOfWeek + 1;
+  }
+
+  const rows: number[][] = [];
+  for (let i = 0; i < LINE_COUNT; i++) {
+    const start = i * 7;
+    const end = start + 7;
+    const row = days.slice(start, end);
+    rows.push(row);
+  }
+
+  return rows;
+};
+
 export const getWeekClass = (order: number) => {
-  if (order === SUNDAY) return "sun";
-  if (order === SATURDAY) return "sat";
+  if (order === SUNDAY) return SUNDAY_CLASS_NAME;
+  if (order === SATURDAY) return SATURDAY_CLASS_NAME;
   return "";
 };
