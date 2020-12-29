25ff23cb29c84b2098fed2e1507148fba02e6dd2
Yoon <yty3592@gmail.com>
2020-12-29 20:55:10 +0900

    utils 정리
    - 각 연도의 월의 마지막 날짜 계산 함수 추가
    - 주말인지 판단하여 스타일을 지정하는 함수 추가
    - 현재 월의 1일의 요일 구하는 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,28 @@
+import { END_DAYS, SUNDAY, SATURDAY } from "../constants";
+
 export const isLeapYear = (year: number) => {
   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+};
+
+export const getEndDays = (currentYear: number) => {
+  const copiedMonths = [...END_DAYS];
+  if (isLeapYear(currentYear)) copiedMonths[1] = 29;
+  return [...copiedMonths];
+};
+
+export const getFirstDayIndexOfWeek = (year: number, month: number) => {
+  const endDays = getEndDays(year);
+  const prevYear = year - 1;
+  let days = prevYear * 365 + Math.floor(prevYear / 4) - Math.floor(prevYear / 100) + Math.floor(prevYear / 400);
+
+  for (let i = 0; i < month - 1; i++) days += endDays[i];
+  days += 1;
+
+  return days % 7;
+};
+
+export const getWeekClass = (order: number) => {
+  if (order === SUNDAY) return "sun";
+  if (order === SATURDAY) return "sat";
+  return "";
 };
